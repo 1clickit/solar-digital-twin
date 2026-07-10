e4d7dfd3be4a7c06beab370f39c443bf9e28f5c0
Chris Gardner <chris@thegardners.us>
2026-07-10 19:12:24 +0000

    Regenerate EG4 portal during refresh

--- eg4_refresh_report.sh
+++ eg4_refresh_report.sh
@@ -5,3 +5,4 @@ DATE_TEXT="${1:-$(date +%F)}"
 
 python src/solar_digital_twin/collectors/eg4/eg4_sync.py --date "$DATE_TEXT"
 python src/solar_digital_twin/reporting/eg4_daily_report.py
+python -m solar_digital_twin.reporting.eg4_portal
